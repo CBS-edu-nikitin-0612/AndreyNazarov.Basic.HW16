5f2b8defb1bfc770a99eb2078c8537127c479b68
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-22 15:45:11 +0300

    feat:Task3

---
+++ Task3/House.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace Task3
+{
+    internal class House : ICloneable
+    {
+        private string name;
+        private string manufacurer;
+        private ArrayList sizes;
+
+        public House(string name, string manufacurer, int height, int width)
+        {
+            this.name = name;
+            this.manufacurer = manufacurer;
+            Height = height;
+            Width = width;
+            sizes = new ArrayList();
+        }
+
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public void Update()
+        {
+            name += "-";
+            manufacurer += "-";
+            Height++;
+            Width++;
+            sizes = new ArrayList();
+        }
+        // Почему результат одинаковый для обоих методов, мне казалось что при использовании
+        // MemberwiseClone на строковые с ссылочные типы должна была скопироваться ссылка
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public House DeepClone()
+        {
+            House clone = (House)MemberwiseClone();
+            clone.name = string.Copy(name);
+            clone.manufacurer = string.Copy(manufacurer);
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name} Manufacturer: {manufacurer} Height: {Height} Width: {Width} Sizes: {sizes.GetHashCode()}";
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("До обновления");
+            House house = new House("House1", "ASD", 5, 5);
+            House houseClone = (House)house.Clone();
+            House houseDeepClone = house.DeepClone();
+            Console.WriteLine(house);
+            Console.WriteLine(houseClone);
+            Console.WriteLine(houseDeepClone);
+            Console.WriteLine("\nПосле обновленния");
+            house.Update();
+            Console.WriteLine(house);
+            Console.WriteLine(houseClone);
+            Console.WriteLine(houseDeepClone);
+        }
+    }
+}

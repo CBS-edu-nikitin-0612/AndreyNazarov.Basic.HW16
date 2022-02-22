47033b6da2ebde6b25230c01982ea6ff4ee5151b
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-22 18:03:34 +0300

    feat:Task6

---
+++ Task6/Point.cs
@@ -0,0 +1,25 @@
+﻿namespace Task6
+{
+    internal struct Point
+    {
+        public Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public static Point operator +(Point point1, Point point2)
+        {
+            return new Point(point1.X + point2.X, point1.Y + point2.Y, point1.Z + point2.Z);
+        }
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}, Z:{Z}";
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(10, 10, 10);
+            Point point2 = new Point(20, 20, 20);
+
+            Console.WriteLine(point1);
+            Console.WriteLine(point2);
+            point1 = point2 + point1;
+            Console.WriteLine(point1);
+        }
+    }
+}

78b5705bd15e5aabcdb2588f4c99b809ea4346ae
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-22 17:52:50 +0300

    feat: Task4

---
+++ Task4/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class MyClass
+    {
+        public DateTime date { get; set; }
+
+        public static double operator -(MyClass inst1, MyClass inst2)
+        {
+            return (inst1.date - inst2.date).TotalDays;
+        }
+        public static MyClass operator +(MyClass myClass, double days)
+        {
+            return new MyClass() { date = myClass.date.AddDays(days) };
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass1 = new MyClass() { date = DateTime.Parse("22/02/2022") };
+            MyClass myClass2 = new MyClass() { date = DateTime.Parse("20/02/2022") };
+            Console.WriteLine(myClass1 - myClass2);
+            myClass1 += 10;
+            Console.WriteLine(myClass1 - myClass2);
+        }
+    }
+}
